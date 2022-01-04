d6aa54dcbff8871845253e92be9d85f16685bd4a
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2022-01-04 23:56:27 +0700

    fix lint

--- src/static/toParam.ts
+++ src/static/toParam.ts
@@ -1,91 +1,86 @@
-import { isFunction } from "../utils/is";
+import { isFunction, isPlainObject } from "../utils/is";
 
 import each from "./each";
 
-const rbracket = /\[\]$/
+const rbracket = /\[\]$/;
 
-
-function buildParams( prefix: string, obj: any, traditional: boolean, add: (key: string, value: any) => void ): void {
-	
-	if ( Array.isArray( obj ) ) {
-
-		// Serialize array item.
-		each( obj, ( v, i ) => {
-			if ( traditional || rbracket.test( prefix ) ) {
-
-				// Treat each array item as a scalar.
-				add( prefix, v );
-
-			} else {
-
-				// Item is non-scalar (array or object), encode its numeric index.
-				buildParams(
-					prefix + "[" + ( typeof v === "object" && v != null ? i : "" ) + "]",
-					v,
-					traditional,
-					add
-				);
-			}
-		} );
-
-	} else if ( !traditional && obj && typeof ( obj ) === "object" ) {
-
-		// Serialize object item.
-		for ( const name in obj ) {
-			buildParams( prefix + "[" + name + "]", obj[ name ], traditional, add );
-		}
-
-	} else {
-
-		// Serialize scalar item.
-		add( prefix, obj );
-	}
+function buildParams(
+  prefix: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  obj: any,
+  traditional: boolean,
+  add: (key: string, value: any) => void
+): void {
+  if (Array.isArray(obj)) {
+    // Serialize array item.
+    each(obj, (v, i) => {
+      if (traditional || rbracket.test(prefix)) {
+        // Treat each array item as a scalar.
+        add(prefix, v);
+      } else {
+        // Item is non-scalar (array or object), encode its numeric index.
+        buildParams(
+          prefix + "[" + (typeof v === "object" && v != null ? i : "") + "]",
+          v,
+          traditional,
+          add
+        );
+      }
+    });
+  } else if (!traditional && obj && typeof obj === "object") {
+    // Serialize object item.
+    for (const name in obj) {
+      buildParams(prefix + "[" + name + "]", obj[name], traditional, add);
+    }
+  } else {
+    // Serialize scalar item.
+    add(prefix, obj);
+  }
 }
 
-function toParam( data?:
+function toParam(
+  data?:
     | {
-        name: string;
-        value: any
+        name: string | number;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        value: any;
       }[]
     | {
-        [key: string | number]: any
-      } | Kijs, traditional?: boolean ): string {
-	const
-		s = new Set<string>(),
-		add = function( key: string, valueOrFunction: any ) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        [key: string | number]: any;
+      }
+    | Kijs,
+  traditional?: boolean
+): string {
+  const s = new Set<string>(),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    add = function (key: string, valueOrFunction: any) {
+      const value = isFunction(valueOrFunction)
+        ? valueOrFunction()
+        : valueOrFunction;
 
-			// If value is a function, invoke it and use its return value
-			const value = isFunction( valueOrFunction ) ?
-				valueOrFunction() :
-				valueOrFunction;
+      s.add(
+        encodeURIComponent(key) +
+          "=" +
+          encodeURIComponent(value == null ? "" : value)
+      );
+    };
 
-			s.add(encodeURIComponent( key ) + "=" +
-				encodeURIComponent( value == null ? "" : value ));
-		};
+  if (data == null) {
+    return "";
+  }
 
-	if ( data == null ) {
-		return "";
-	}
+  if (Array.isArray(data) || (data.kijs && !isPlainObject(data))) {
+    each(data, ({ name, value }) => {
+      add(name, value);
+    });
+  } else {
+    for (const prefix in data) {
+      buildParams(prefix, data[prefix], traditional, add);
+    }
+  }
 
-	// If an array was passed in, assume that it is an array of form elements.
-	if ( Array.isArray( data ) || ( data.kijs && !isPlainObject( data ) ) ) {
-
-		// Serialize the form elements
-		each( data, ({ name, value }) => {
-			add( name, value );
-		} );
-
-	} else {
-
-		// If traditional, encode the "old" way (the way 1.3.2 or older
-		// did it), otherwise encode params recursively.
-		for ( const prefix in data ) {
-			buildParams( prefix, data[ prefix ], traditional, add );
-		}
-	}
-
-	// Return the resulting serialization
-	return s.join( "&" );
-};
+  return s.join("&");
+}
 
 export default toParam;
--- src/utils/is.ts
+++ src/utils/is.ts
@@ -22,22 +22,27 @@ export function isObject(obj: any) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isPlainObject<T extends Record<any, any>>( obj: any ): obj is T {
+export function isPlainObject<T extends Record<any, any>>(obj: any): obj is T {
+  // Detect obvious negatives
+  // Use toString instead of jQuery.type to catch host objects
+  if (!obj || Object.prototype.toString.call(obj) !== "[object Object]") {
+    return false;
+  }
 
-		// Detect obvious negatives
-		// Use toString instead of jQuery.type to catch host objects
-		if ( !obj || Object.prototype.toString.call(obj) !== "[object Object]" ) {
-			return false;
-		}
+  const proto = Object.getPrototypeOf(obj);
 
-		const proto = Object.getPrototypeOf( obj );
+  // Objects with no prototype (e.g., `Object.create( null )`) are plain
+  if (!proto) {
+    return true;
+  }
 
-		// Objects with no prototype (e.g., `Object.create( null )`) are plain
-		if ( !proto ) {
-			return true;
-		}
-
-		// Objects with prototype are plain iff they were constructed by a global Object function
-		const Ctor = Object.prototype.hasOwnProperty.call( proto, "constructor" ) && proto.constructor;
-		return typeof Ctor === "function" && Object.prototye.hasOwnProperty.toString.call( Ctor ) === Object.prototye.hasOwnProperty.toString.call( Object );
-	}
+  // Objects with prototype are plain iff they were constructed by a global Object function
+  const Ctor =
+    Object.prototype.hasOwnProperty.call(proto, "constructor") &&
+    proto.constructor;
+  return (
+    typeof Ctor === "function" &&
+    Object.prototye.hasOwnProperty.toString.call(Ctor) ===
+      Object.prototye.hasOwnProperty.toString.call(Object)
+  );
+}
