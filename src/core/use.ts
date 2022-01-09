411721684123a7ec134ee0c813c5dc77bb65aa6d
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2022-01-09 08:07:49 +0700

    LINT

--- src/core/use.ts
+++ src/core/use.ts
@@ -1,18 +1,18 @@
 import { Kijs } from "./kijs";
 import { isFunction } from "../utils/is";
 
-type Plugin<T> = ((
-  Ki: typeof Kijs,
-  ...option: readonly T[]
-) =>
-  | void)
+type Plugin<T> =
+  | ((Ki: typeof Kijs, ...option: readonly T[]) => void)
   | (any & {
       install: (Ki: typeof Kijs, ...option: readonly T[]) => void;
     });
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 const installedPlugins = new WeakSet<Plugin<any>>();
 
-function use<T = void>(plugin: Plugin<T>, ...options: readonly T[]): typeof Kijs {
+function use<T = void>(
+  plugin: Plugin<T>,
+  ...options: readonly T[]
+): typeof Kijs {
   if (installedPlugins.has(plugin)) {
     console.warn(`Plugin has already been applied to target app.`);
   } else if (plugin && isFunction(plugin.install)) {
