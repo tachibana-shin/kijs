cdcc7945dcd2554417ab7d8af475e62e81dd6cc3
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2022-01-09 07:57:15 +0700

    lint

--- src/core/use.ts
+++ src/core/use.ts
@@ -1,18 +1,21 @@
 import { Kijs } from "./kijs";
 import { isFunction } from "../utils/is";
 
-type Plugin < T > = (Ki: typeof Kijs, ...option ? : T) => void | {
-  // eslint-disable-next-line functional/prefer-readonly-type, @typescript-eslint/no-explicit-any
-  [key: string]: any
-} & {
-  install: (Ki: typeof Kijs, ...option ? : T) => void
-}
+type Plugin<T> = (
+  Ki: typeof Kijs,
+  ...option: T
+) =>
+  | void
+  | ({
+      // eslint-disable-next-line functional/prefer-readonly-type, @typescript-eslint/no-explicit-any
+      [key: string]: any;
+    } & {
+      install: (Ki: typeof Kijs, ...option: T) => void;
+    });
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 const installedPlugins = new WeakSet<Plugin<any>>();
 
-function use(
-  plugin: Plugin < T > ,
-  ...options ? : T
-): typeof Kijs {
+function use(plugin: Plugin<T>, ...options: T): typeof Kijs {
   if (installedPlugins.has(plugin)) {
     console.warn(`Plugin has already been applied to target app.`);
   } else if (plugin && isFunction(plugin.install)) {
@@ -22,7 +25,10 @@ function use(
     installedPlugins.add(plugin);
     plugin(app, ...options);
   } else {
-    console.warn(`A plugin must either be a function or an object with an "install" ` + `function.`);
+    console.warn(
+      `A plugin must either be a function or an object with an "install" ` +
+        `function.`
+    );
   }
 
   return Kijs;
